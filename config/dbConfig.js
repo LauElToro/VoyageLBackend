81d6b08d609fc8794fa6c4c429137dd47ab8625e
LauElToro <110632478+LauElToro@users.noreply.github.com>
2024-05-08 02:30:31 -0300

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 // Configura la conexión a la base de datos MySQL
 const db = mysql.createPool({
   host: 'sql11.freesqldatabase.com',
-  user: 'sql11703862',
-  password: 'Bf91liueDk',
-  database: 'sql11703862'
+  user: 'sql11704922',
+  password: 'USY5uQcRBr',
+  database: 'sql11704922'
 });
 
 module.exports = db;
